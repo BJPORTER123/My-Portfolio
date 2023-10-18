61928357d2fff7114f677cb0a185069be7e5e919
Ben <bjporter123@gmail.com>
2023-10-18 17:51:12 +0100

    fixed linked

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,13 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import { HashLink } from 'react-router-hash-link';
-import {BrowserRouter} from "react-router-dom";
-import profile from "../assets/img/profilephoto.png"
-import linkedIn from "../assets/img/linkedin.png"
-import github from "../assets/img/github.png"
+import { HashLink } from "react-router-hash-link";
+import { BrowserRouter } from "react-router-dom";
+import profile from "../assets/img/profilephoto.png";
+import linkedIn from "../assets/img/linkedin.png";
+import github from "../assets/img/github.png";
 
 const NavBar = () => {
-
-  const [activeLink, setActiveLink] = useState('home');
+  const [activeLink, setActiveLink] = useState("home");
   const [scrolled, setScrolled] = useState(false);
 
   useEffect(() => {
@@ -18,16 +17,16 @@ const NavBar = () => {
       } else {
         setScrolled(false);
       }
-    }
+    };
 
     window.addEventListener("scroll", onScroll);
 
     return () => window.removeEventListener("scroll", onScroll);
-  }, [])
+  }, []);
 
   const onUpdateActiveLink = (value) => {
     setActiveLink(value);
-  }
+  };
 
   return (
     <BrowserRouter>
@@ -41,25 +40,62 @@ const NavBar = () => {
           </Navbar.Toggle>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
-              <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
-              <Nav.Link href="https://drive.google.com/file/d/16jrzeqnAY4DgvFZNKZ4BKykCAQYM2M0m/view?usp=share_link" className="navbar-link">Resume</Nav.Link>
+              <Nav.Link
+                href="#home"
+                className={
+                  activeLink === "home" ? "active navbar-link" : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("home")}
+              >
+                Home
+              </Nav.Link>
+              <Nav.Link
+                href="#skills"
+                className={
+                  activeLink === "skills" ? "active navbar-link" : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("skills")}
+              >
+                Skills
+              </Nav.Link>
+              <Nav.Link
+                href="#projects"
+                className={
+                  activeLink === "projects"
+                    ? "active navbar-link"
+                    : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("projects")}
+              >
+                Projects
+              </Nav.Link>
+              <Nav.Link
+                href="https://drive.google.com/file/d/178ZLUSDAsook5EvzZ84GLETt07VSYGqB/view?usp=sharing"
+                className="navbar-link"
+              >
+                Resume
+              </Nav.Link>
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="https://www.linkedin.com/in/ben-porter-8b52a2266/"><img src={linkedIn} alt="" /></a>
-                <a href="https://github.com/BJPORTER123"><img src={github} alt="" /></a>
+                <a href="https://www.linkedin.com/in/ben-porter-8b52a2266/">
+                  <img src={linkedIn} alt="" />
+                </a>
+                <a href="https://github.com/BJPORTER123">
+                  <img src={github} alt="" />
+                </a>
               </div>
-              <HashLink to='#connect'>
-                <button className="vvd"><span>Let’s Connect</span></button>
+              <HashLink to="#connect">
+                <button className="vvd">
+                  <span>Let’s Connect</span>
+                </button>
               </HashLink>
             </span>
           </Navbar.Collapse>
         </Container>
       </Navbar>
     </BrowserRouter>
-  )
-}
+  );
+};
 
 export default NavBar;
